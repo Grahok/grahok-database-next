174b1bf5c2597d4e0ada70d0102022a2172d2ed1
Shakil Ahmmed <shakilahmmedwakil.saw+one@gmail.com>
2025-04-13 02:40:24 +0600

    Created Simple Dashboard

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,77 @@
+import {
+  FaBox,
+  FaBoxesStacked,
+  FaCartPlus,
+  FaCirclePlus,
+  FaDatabase,
+  FaUpRightFromSquare,
+  FaUser,
+  FaUserPlus,
+} from "react-icons/fa6";
+
 export default function Home() {
-  return <h1 className="text-4xl font-bold text-blue-600">Grahok Database</h1>;
+  return (
+    <section className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-6">
+      <a
+        href="/entries/all"
+        className="flex items-center justify-between gap-3 bg-gray-100 p-4 rounded-lg shadow-md hover:bg-gray-200 transition group h-20"
+      >
+        <div className="flex items-center gap-3 flex-1">
+          <FaDatabase className="size-5 flex-shrink-0" />
+          <p className="whitespace-normal leading-tight">All Entries</p>
+        </div>
+        <FaUpRightFromSquare className="hidden group-hover:block transition-transform duration-300 ease-in-out size-5 flex-shrink-0" />
+      </a>
+      <a
+        href="/entries/add"
+        className="flex items-center justify-between gap-3 bg-gray-100 p-4 rounded-lg shadow-md hover:bg-gray-200 transition group h-20"
+      >
+        <div className="flex items-center gap-3 flex-1">
+          <FaCirclePlus className="size-5 flex-shrink-0" />
+          <p className="whitespace-normal leading-tight">Add Entry</p>
+        </div>
+        <FaUpRightFromSquare className="hidden group-hover:block transition-transform duration-300 ease-in-out size-5 flex-shrink-0" />
+      </a>
+      <a
+        href="/customers/all"
+        className="flex items-center justify-between gap-3 bg-gray-100 p-4 rounded-lg shadow-md hover:bg-gray-200 transition group h-20"
+      >
+        <div className="flex items-center gap-3 flex-1">
+          <FaUser className="size-5 flex-shrink-0" />
+          <p className="whitespace-normal leading-tight">All Customers</p>
+        </div>
+        <FaUpRightFromSquare className="hidden group-hover:block transition-transform duration-300 ease-in-out size-5 flex-shrink-0" />
+      </a>
+      <a
+        href="/customers/add"
+        className="flex items-center justify-between gap-3 bg-gray-100 p-4 rounded-lg shadow-md hover:bg-gray-200 transition group h-20"
+      >
+        <div className="flex items-center gap-3 flex-1">
+          <FaUserPlus className="size-5 flex-shrink-0" />
+          <p className="whitespace-normal leading-tight">Add Customer</p>
+        </div>
+        <FaUpRightFromSquare className="hidden group-hover:block transition-transform duration-300 ease-in-out size-5 flex-shrink-0" />
+      </a>
+      <a
+        href="/products/all"
+        className="flex items-center justify-between gap-3 bg-gray-100 p-4 rounded-lg shadow-md hover:bg-gray-200 transition group h-20"
+      >
+        <div className="flex items-center gap-3 flex-1">
+          <FaBoxesStacked className="size-5 flex-shrink-0" />
+          <p className="whitespace-normal leading-tight">All Products</p>
+        </div>
+        <FaUpRightFromSquare className="hidden group-hover:block transition-transform duration-300 ease-in-out size-5 flex-shrink-0" />
+      </a>
+      <a
+        href="/products/add"
+        className="flex items-center justify-between gap-3 bg-gray-100 p-4 rounded-lg shadow-md hover:bg-gray-200 transition group h-20"
+      >
+        <div className="flex items-center gap-3 flex-1">
+          <FaCirclePlus className="size-5 flex-shrink-0" />
+          <p className="whitespace-normal leading-tight">Add Product</p>
+        </div>
+        <FaUpRightFromSquare className="hidden group-hover:block transition-transform duration-300 ease-in-out size-5 flex-shrink-0" />
+      </a>
+    </section>
+  );
 }
