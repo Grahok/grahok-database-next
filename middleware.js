0edd9d195e97ca963b5002d9bc57c214bbadbdab
Shakil Ahmmed <shakilahmmedwakil.saw+one@gmail.com>
2025-05-29 16:35:27 +0600

    Bug Fixes

--- middleware.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function middleware(request) {
-    // Do nothing, just by existing it disables static optimization
-    return NextResponse.next();
-  }
-  
