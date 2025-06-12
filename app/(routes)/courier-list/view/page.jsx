0c9b1496ab9661bb9642de15468562a340b6126b
Shakil Ahmmed <shakilahmmedwakil.saw+one@gmail.com>
2025-06-12 17:03:43 +0600

    Convert courier list to client component for now.

--- app/(routes)/courier-list/view/page.jsx
+++ app/(routes)/courier-list/view/page.jsx
@@ -1,12 +1,23 @@
-export const dynamic = "force-dynamic";
+"use client"
 
 import fetchCourierLists from "@/features/courier-list/actions/fetchCourierLists";
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
+import { useEffect, useState } from "react";
 
-export default async function ViewCourierInfo() {
-  const response = await fetchCourierLists();
-  const { courierInfo } = await response.json();
+export default function ViewCourierInfo() {
+  const [courierInfo, setCourierInfo] = useState([]);
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetchCourierLists();
+        const { courierInfo } = await response.json();
+        setCourierInfo(courierInfo);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  });
 
   return (
     <div className="container mx-auto py-10">
