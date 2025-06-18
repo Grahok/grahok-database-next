8eec416c22a938b5cdcfc2b9ab6ab64c3a761407
Shakil Ahmmed <shakilahmmedwakil.saw+one@gmail.com>
2025-06-18 13:07:31 +0600

    Static page fix

--- app/(routes)/courier-list/view/page.jsx
+++ app/(routes)/courier-list/view/page.jsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import fetchCourierLists from "@/features/courier-list/actions/fetchCourierLists";
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
